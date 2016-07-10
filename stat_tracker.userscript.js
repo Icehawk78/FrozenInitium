321a2499ce5715b37253c764566a10c20bd713c3
Nicholas Laux <i.am@aweso.ms>
2016-07-10 01:35:08 -0500

    Update and rename stat_tracker.js to stat_tracker.userscript.js

--- stat_tracker.userscript.js
+++ stat_tracker.userscript.js
@@ -1,8 +1,16 @@
-function parseData(key, default) {
+// ==UserScript==
+// @name            initium-stats
+// @version         0.0.1
+// @match           https://www.playinitium.com/*
+// @match           http://www.playinitium.com/*
+// @grant           none
+// ==/UserScript==
+
+function parseData(key, defaultVal) {
 	var value = localStorage.getItem(key);
-	if (typeof(value) == 'undefined' || value == null) {
-		storeData(key, default);
-		value = default;
+	if (typeof(value) == 'undefined' || value === null) {
+		storeData(key, defaultVal);
+		value = defaultVal;
 	} else {
 		value = JSON.parse(value);
 	}
@@ -20,33 +28,35 @@ function getStats(type, initial, current, hits) {
 	var mod, modIncrement;
 	var max, minMax;
 
-	if (type == "str") {
+	if (statSelect()=="str") {
 		mod=0.0009954;
 		max=11;
 		minMax=9;
 		modIncrement=0.0000014965;
 //		modMax=0.0012947;
-	} else if (type == "dex") {
+	}
+	else if (statSelect()=="dex") {
 		mod=0.00057334;
 		max=10;
 		minMax=8;
 		modIncrement=0.0000007418493;
 //		modMax=0.00072171;
-	} else if (type == "int") {
+	}
+	else if (statSelect()=="int") {
 		mod=0.0001414791;
 		max=10;
 		minMax=8;
 		modIncrement=0.0000002897295;
 //		modMax=0.000199425;
 	}
-	
+
 	return {
-		max: evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax), 
+		max: evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax),
 		min: evalminStats(initial, current, hits, mod, modIncrement, max, minMax)
 	};
 }
 
-function evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax) {		
+function evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax) {
 	for (var g = max; g >= minMax; g -= 0.01) {
 		var tracked = initial;
 		for (var i = 1; i <= hits; i++) {
@@ -59,7 +69,7 @@ function evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax) {
 	}
 }
 
-function evalminStats(initial, current, hits, mod, modIncrement, max, minMax) {		
+function evalminStats(initial, current, hits, mod, modIncrement, max, minMax) {
 	for (var g = minMax; g <= max; g += 0.01) {
 		var tracked = initial;
 		for (var i = 1; i <= hits; i++) {
@@ -81,23 +91,23 @@ $.get($('.character-display-box').find('a').first().attr('rel'), function(res) {
 		dex: parseFloat(response.find('[name=dexterity]').text().split(' ')[0]),
 		int: parseFloat(response.find('[name=intelligence]').text().split(' ')[0])
 	};
-	var defaultData = {str: {initial: currentStats['str'], current: currentStats['str'], hits: 0}, dex: {initial: currentStats['dex'], current: currentStats['dex'], hits: 0}, int: {initial: currentStats['int'], current: currentStats['int'], hits: 0}};
+	var defaultData = {str: {initial: currentStats.str, current: currentStats.str, hits: 0}, dex: {initial: currentStats.dex, current: currentStats.dex, hits: 0}, int: {initial: currentStats.int, current: currentStats.int, hits: 0}};
 	myStats = parseData(characterName, defaultData);
-	if (myStats['str'].current != currentStats['str'] || myStats['dex'].current != currentStats['dex'] || myStats['int'].current != currentStats['int']) {
+	if (myStats.str.current != currentStats.str || myStats.dex.current != currentStats.dex || myStats.int.current != currentStats.int) {
 		myStats = defaultData;
 		storeData(characterName, myStats);
 	}
 	['str', 'dex', 'int'].forEach(function(type) {
 		var stats = getStats(type, myStats[type].initial, myStats[type].current, myStats[type].hits);
 		console.log('[' + type + '] Min: ', stats.min, ' Max: ', stats.max);
-	})
+	});
 });
 
 function doAttack(hand) {
-	if (myStats == null) {
+	if (myStats === null) {
 		return setTimeout(function() {doAttack(hand);}, 1000);
 	} else {
-		data = {type: 'attack', hand: hand, v: window.verifyCode};
+		var data = {type: 'attack', hand: hand, v: window.verifyCode};
 		$.get('ServletCharacterControl', data, function() {
 			$.get($('.character-display-box').find('a').first().attr('rel'), function(res) {
 				var response = $($.parseHTML(res));
