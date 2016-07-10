cbc941bf2e22c6b405275c7c0daca6c2102d0b05
Nicholas Laux <i.am@aweso.ms>
2016-07-10 00:21:38 -0500

    Update stat_tracker.js

--- stat_tracker.js
+++ stat_tracker.js
@@ -13,8 +13,69 @@ function storeData(key, value) {
   localStorage.putItem(key, JSON.stringify(value));
 }
 
+function getStats(type, initial, current, hits) {
+	var stats;
+//	var compmod = (initial == 5) ? 0.005 : 0;
+//	var diff = current - initial - compmod;
+	var mod, modIncrement;
+	var max, minMax;
+
+	if (statSelect()=="str") {
+		mod=0.0009954;
+		max=11;
+		minMax=9;
+		modIncrement=0.0000014965;
+//		modMax=0.0012947;
+	}
+	else if (statSelect()=="dex") {
+		mod=0.00057334;
+		max=10;
+		minMax=8;
+		modIncrement=0.0000007418493;
+//		modMax=0.00072171;
+	}
+	else if (statSelect()=="int") {
+		mod=0.0001414791;
+		max=10;
+		minMax=8;
+		modIncrement=0.0000002897295;
+//		modMax=0.000199425;
+	}
+	
+	return {
+	  max: evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax), 
+	  min: evalminStats(initial, current, hits, mod, modIncrement, max, minMax)
+	};
+}
+
+function evalmaxStats(initial, current, hits, mod, modIncrement, max, minMax) {		
+	for (var g = max; g >= minMax; g -= 0.01) {
+		var tracked = initial;
+		for (var i = 1; i <= hits; i++) {
+			tracked = tracked + (g - tracked) * mod;
+		}
+		if (((Math.round(tracked*100))/100) == current) {
+		  return g;
+		}
+		mod += modIncrement;
+	}
+}
+
+function evalminStats(initial, current, hits, mod, modIncrement, max, minMax) {		
+	for (var g = minMax; g <= max; g += 0.01) {
+		var tracked = initial;
+		for (var i = 1; i <= hits; i++) {
+			tracked = tracked + (g - tracked) * mod;
+		}
+		if (((Math.round(tracked*100))/100) == current) {
+		  return g;
+		}
+		mod += modIncrement;
+	}
+}
 
 var characterName = $('[rel=#profile]').text();
+var myStats = null;
 $.get($('.character-display-box').find('a').first().attr('rel'), function(res) {
   var response = $($.parseHTML(res));
   var currentStats = {
@@ -22,5 +83,25 @@ $.get($('.character-display-box').find('a').first().attr('rel'), function(res) {
     dex: parseFloat(response.find('[name=dexterity]').text().split(' ')[0]),
     int: parseFloat(response.find('[name=intelligence]').text().split(' ')[0])
   };
-  var myStats = parseData(characterName, 
-})
+  var defaultData = {current: currentStats, hits: 0, str: {initial: currentStats['str'], min: 9, max: 11}, dex: {initial: currentStats['dex'], min: 8, max: 10}, int: {initial: currentStats['int'], min: 8, max: 10}};
+  myStats = parseData(characterName, defaultData);
+  if (myStats.current['str'] != currentStats['str'] || myStats.current['dex'] != currentStats['dex'] || myStats.current['int'] != currentStats['int']) {
+    myStats = defaultData;
+    storeData(characterName, myStats);
+  }
+});
+
+function doAttack(hand) {
+  if (myStats == null) {
+    return setTimeout(function() {doAttack(hand);}, 1000);
+  }
+  data = {type: 'attack', hand: hand, v: window.verifyCode};
+}
+
+function combatAttackWithLeftHand() {
+  doAttack('LeftHand');
+}
+
+function combatAttackWithRightHand() {
+  doAttack('RightHand');
+}
