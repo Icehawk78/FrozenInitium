00d4964b8a843b2721078945e504482a635e628d
Nicholas Laux <i.am@aweso.ms>
2016-07-12 21:37:49 -0500

    Update auto_explore.js

--- auto_explore.js
+++ auto_explore.js
@@ -1,14 +1,17 @@
 var data = {type: 'explore_ajax', ignoreCombatSites: true, v: window.verifyCode};
 var times = 0;
 var currentLocation = $('.header-location').text();
-var doExplore = function() {
+doExplore = function() {
+  showBannerLoadingIcon();
   $.getJSON('ServletCharacterControl', data, function(res) {
-    if (res['locationName'] == currentLocation) {
+    if (res.locationName == currentLocation) {
       times += 1;
+      exploreTimes += res.isComplete ? 1 : 0;
       console.log('Not found, explored', times, 'times.');
-      setTimeout(doExplore, (res['isComplete'] ? 1000 : res['timeLeft'] + 1 * 1000));
+      $('.main-dynamic-content-box.paragraph').text('Not found, attempted ' + times + ' times, explored ' + exploreTimes + ' times. (res.isComplete: ' + res.isComplete + ', res.timeLeft: ' + res.timeLeft + ')');
+      setTimeout(doExplore, (res.timeLeft + 1) * 1000);
     } else {
-      if (res['error']) {
+      if (res.error) {
         window.location = 'combat.jsp';
       } else {
         window.location = 'main.jsp';
